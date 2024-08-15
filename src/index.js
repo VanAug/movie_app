fb2b2578ef71df7dda681c7956105b19ec4f473c
VanAug <okellobrian18@gmail.com>
2024-08-15 09:02:09 +0300

    File and folder structure

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/App';
 
-import { createRoot } from 'react-dom/client';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
-root.render(<App />);
+root.render(
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+);
