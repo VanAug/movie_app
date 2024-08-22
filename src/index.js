f1062ba2428434423a1155ff6bb996f59e75b1bb
VanAug <okellobrian18@gmail.com>
2024-08-22 08:29:07 +0300

    Movie Display

--- src/index.js
+++ src/index.js
@@ -2,15 +2,23 @@ import React from 'react';
 // import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material';
+import { Provider } from 'react-redux';
 
 import App from './components/App';
+import store from './app/store';
 
+const theme = createTheme({});
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
+    <Provider store={store}>
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </ThemeProvider>,
+    </Provider>
 );
---
+++ src/services/TMDB.js
@@ -0,0 +1,28 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const tmdbApiKey = process.env.REACT_APP_TMDB_KEY;
+const page = 1;
+// https://api.themoviedb.org/3/movie/popular?language=en-US&page=1
+// https://api.themoviedb.org/3/genre/movie/list?language=en
+
+
+export const tmdbApi = createApi({
+    reducerPath: 'tmdApi',
+    baseQuery: fetchBaseQuery({ baseUrl: ' https://api.themoviedb.org/3'}),
+    endpoints: (builder) => ({
+        // Get Genres
+        getGenres: builder.query({
+            query: () => `genre/movie/list?api_key=${tmdbApiKey}`
+        }),
+
+        // Get movies by type
+        getMovies: builder.query({
+            query: () => `movie/popular?page=${page}&api_key=${tmdbApiKey}`,
+        }),
+    }),
+});
+
+export const {
+    useGetGenresQuery,
+    useGetMoviesQuery,
+} = tmdbApi;
