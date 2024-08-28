7b09750b1c5b19f4cc27e9fb4bb88d68404ed28f
VanAug <okellobrian18@gmail.com>
2024-08-28 10:21:39 +0300

    Responsive adjustments

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -49,7 +49,7 @@ const MovieInformation = () => {
 
   return (
     <Grid container className={classes.containerSpaceAround}>
-      <Grid item sm={12} lg={4}>
+      <Grid item sm={12} lg={4} >
         <img 
           className={classes.poster}
           src={`https://image.tmdb.org/t/p/w500/${data?.poster_path}`}
--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -12,7 +12,7 @@ const Movies = () => {
   const [page, setPage] = useState(1);
   const { genreIdOrCategoryName, searchQuery } = useSelector((state) => state.currentGenreOrCategory);
   const { data, error, isFetching } = useGetMoviesQuery({ genreIdOrCategoryName, page, searchQuery });
-  const lg = useMediaQuery((theme) => theme.breakpoints.omly('lg'));
+  const lg = useMediaQuery((theme) => theme.breakpoints.only('lg'));
 
   const numberOfMovies = lg ? 16 : 18;
 
